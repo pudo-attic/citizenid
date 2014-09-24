00aae39dfe94c42b261929b93b102f757b6591f1
Friedrich Lindenberg <friedrich@pudo.org>
2014-09-24 16:48:47 -0700

    break out the config

--- citizenid/app.py
+++ citizenid/app.py
@@ -1,15 +1,14 @@
 # coding: utf-8
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
 from flask_oauthlib.provider import OAuth2Provider
 
+from citizenid import default_settings
 
-app = Flask(__name__, template_folder='templates')
-app.debug = True
-app.secret_key = 'secret'
-app.config.update({
-    'SQLALCHEMY_DATABASE_URI': 'sqlite:///db.sqlite',
-})
+app = Flask(__name__)
+
+app.config.from_object(default_settings)
+app.config.from_envvar('COAF_SETTINGS', silent=True)
+
 db = SQLAlchemy(app)
 oauth = OAuth2Provider(app)
---
+++ citizenid/default_settings.py
@@ -0,0 +1,6 @@
+
+DEBUG = True
+ASSETS_DEBUG = False
+
+SECRET = 'banana'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///db.sqlite'
